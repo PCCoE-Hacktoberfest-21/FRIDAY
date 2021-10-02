1adb2e217db23caea726c7f6571ac539eb9ee08e
hari <haribaheti1256@gmail.com>
2021-10-02 15:27:57 +0530

    Adding News api

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import os
 import time
-import playsound       #pip install playsound
-import speech_recognition as sr   #pip install speech_recognition
-from gtts import gTTS     #pip install gTTS
-import pyaudio        #pip install pyaudio
-import datetime   
+import playsound  # pip install playsound
+import speech_recognition as sr  # pip install speech_recognition
+from gtts import gTTS  # pip install gTTS
+import pyaudio  # pip install pyaudio
+import datetime
+from newsapi import NewsApiClient  # for latest news api
+import credentials
 
 
-
-#function to accept audio input from user#
+# function to accept audio input from user#
 
 def get_audio():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        audio=r.listen(source)
-        said=""
+        audio = r.listen(source)
+        said = ""
         
         try:
             said=r.recognize_google(audio)
@@ -27,28 +28,41 @@ def get_audio():
 #get_audio()
 
 
-#function to convert text_to_speech#
+# function to convert text_to_speech#
 
 def speak(text):
-    tts=gTTS(text=text,lang="en-in")
-    filename="voice2.mp3"
+    tts = gTTS(text=text, lang="en-in")
+    filename = "voice2.mp3"
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
 
 
+# function for News
+def news():
+    # for latest news
+    newsapi = NewsApiClient(api_key='{}'.format(credentials.newsapikey))
+    # /v2/top-headlines
+    top_headlines = newsapi.get_top_headlines(country='in')
+    ranks = ["First", 'Sceond', 'Third', 'Fourth', 'Fifth']
+    for i in range(5):
+        # first five news
+        print(f"{ranks[i]} News : {top_headlines['articles'][i]['title']}")
+        speak(f"{ranks[i]} News : {top_headlines['articles'][i]['title']}".replace(" - ", "news by"))
 
-#Voice_assistant skills#
+
+# Voice_assistant skills#
 
 time.sleep(2)
 speak("Hi what can i do for you?")
 while True:
-        query = get_audio().lower()
-        if 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+    query = get_audio().lower()
+    if 'the time' in query:
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir, the time is {strTime}")
 
-        # add more functinalities below this:
-        
-        else:
-            break
+    # add more functinalities below this:
+    elif 'news' in query:
+        news()
+    else:
+        break
