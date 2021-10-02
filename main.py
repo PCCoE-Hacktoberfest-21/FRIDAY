45036eaae590f8f21b33376b80efa5ecc6a4d5d4
kishores <stkishore851999@gmail.com>
2021-10-02 16:29:14 +0530

    Added active Covid 19 cases support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 
 import playsound  # pip install playsound
+import requests
 import speech_recognition as sr  # pip install speech_recognition
 from gtts import gTTS  # pip install gTTS
 
@@ -30,12 +31,35 @@ def get_audio():
 
 # function to convert text_to_speech#
 
+
 def speak(text):
     tts = gTTS(text=text, lang="en-in")
     filename = "voice2.mp3"
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
+
+
+def check_command_is_for_covid_cases(command):
+    if 'active cases of covid-19' in command:
+        country = get_country(command).capitalize()
+        cases = get_covid_cases(country)
+        speak(f"The current active cases in {country} are {cases}")
+
+
+def get_country(command):
+    country = command.split()[-1]
+    if country == "?":
+        country = command.split()[-2]
+    return country
+
+
+def get_covid_cases(country):
+    totalActiveCases = 0
+    response = requests.get('https://api.covid19api.com/live/country/' + country + '/status/confirmed').json()
+    for data in response:
+        totalActiveCases += data.get('Active')
+    return totalActiveCases
 
 
 # Voice_assistant skills#
@@ -50,6 +74,8 @@ while True:
     if 'the time' in query:
         strTime = datetime.datetime.now().strftime("%H:%M:%S")
         speak(f"Sir, the time is {strTime}")
+
+    check_command_is_for_covid_cases(query)
 
     # add more functinalities below this:
     if last_query:
