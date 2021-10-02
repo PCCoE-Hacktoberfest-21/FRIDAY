4f12ae6eb0b0b693e7407e28a39fce82629af6fe
kishores <stkishore851999@gmail.com>
2021-10-02 14:57:49 +0530

    Formatted main file

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
+import datetime
 import os
 import time
-import playsound       #pip install playsound
-import speech_recognition as sr   #pip install speech_recognition
-from gtts import gTTS     #pip install gTTS
-import pyaudio        #pip install pyaudio
-import datetime   
+
+import playsound  # pip install playsound
+import speech_recognition as sr  # pip install speech_recognition
+from gtts import gTTS  # pip install gTTS
 
 
-
-#function to accept audio input from user#
+# function to accept audio input from user#
 
 def get_audio():
-    r=sr.Recognizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        audio=r.listen(source)
-        said=""
-        
+        audio = r.listen(source)
+        said = ""
+
         try:
-            said=r.recognize_google(audio)
+            said = r.recognize_google(audio)
             print(said)
-            
+
         except Exception as e:
-            print("Exception: "+str(e))
-            
+            print("Exception: " + str(e))
+
     return said
-#get_audio()
 
 
-#function to convert text_to_speech#
+# get_audio()
+
+
+# function to convert text_to_speech#
 
 def speak(text):
-    tts=gTTS(text=text,lang="en-in")
-    filename="voice2.mp3"
+    tts = gTTS(text=text, lang="en-in")
+    filename = "voice2.mp3"
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
 
 
-
-#Voice_assistant skills#
+# Voice_assistant skills#
 
 time.sleep(2)
 speak("Hi what can i do for you?")
@@ -46,18 +46,18 @@ speak("Hi what can i do for you?")
 last_query = None
 
 while True:
-        query = get_audio().lower()
-        if 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            speak(f"Sir, the time is {strTime}")
+    query = get_audio().lower()
+    if 'the time' in query:
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Sir, the time is {strTime}")
 
-        # add more functinalities below this:
-        if last_query:
-            if last_query == 'open notepad':
-                with open('notepad.txt','w+') as file:
-                    file.write(query)
+    # add more functinalities below this:
+    if last_query:
+        if last_query == 'open notepad':
+            with open('notepad.txt', 'w+') as file:
+                file.write(query)
 
-        elif 'open notepad' in query:
-            last_query = 'open notepad'
-        else:
-            break
+    elif 'open notepad' in query:
+        last_query = 'open notepad'
+    else:
+        break
