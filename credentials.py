a1d4923f5dd6265bddc985ba9cfbe0ea5f99129c
Harsh Balaprasad Baheti <harshbaheti441@gmail.com>
2021-10-23 15:55:59 +0530

    adding "unread email feature" credentials

--- credentials.py
+++ credentials.py
@@ -2,3 +2,6 @@ newsapikey = "API KEY "  # https://newsapi.org/register
 contacts = {"ram": "+91xxxxxxxxxx",
             "sham": "+91xxxxxxxxxx"
             }
+# for emails 
+email = xxxxxxxx@xxxx.xxx
+password = XXXXXXXXXXXXXX
