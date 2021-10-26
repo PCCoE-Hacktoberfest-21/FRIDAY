64e39eabe7271e7e2ac0b5ae60dead63a5d7459e
DEDSKULL <51334424+dedskull@users.noreply.github.com>
2021-10-26 16:02:14 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,6 +2,6 @@ newsapikey = "API KEY "  # https://newsapi.org/register
 contacts = {"ram": "+91xxxxxxxxxx",
             "sham": "+91xxxxxxxxxx"
             }
-# for emails 
+# for emails & password :
 email = xxxxxxxx@xxxx.xxx
 password = XXXXXXXXXXXXXX
