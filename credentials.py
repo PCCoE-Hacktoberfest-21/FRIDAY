705726400477e797683881c6d1420dc1161d9596
Harsh Balaprasad Baheti <harshbaheti441@gmail.com>
2021-10-30 10:02:50 +0530

    Adding weather credentials

--- credentials.py
+++ credentials.py
@@ -5,7 +5,8 @@ contacts = {"ram": "+91xxxxxxxxxx",
 # for emails 
 email = xxxxxxxx@xxxx.xxx
 password = XXXXXXXXXXXXXX
-   
+# for weather report
+weatherapikey = "api key" #https://openweathermap.org/api
  application ={
                    'app':'<location of file>'
 #              e.g.'photoshop': 'C:\\"Program Files"\\Adobe\\Adobe Photoshop CC 2018\\Photoshop.exe',
